a955c3a7893dad7b47aa7dea24e621f8172eb412
domds1998 <dominik.suchner@gmail.com>
2020-05-27 14:04:27 +0200

    Added Author Model

---
+++ src/main/java/Models/Author.java
@@ -0,0 +1,50 @@
+package Models;
+
+import java.util.Date;
+
+public class Author {
+
+    private int idAuthor;
+    private String name;
+    private String surname;
+    private Date birthdate;
+
+    public Author(int idAuthor, String name, String surname, Date birthdate) {
+        this.idAuthor = idAuthor;
+        this.name = name;
+        this.surname = surname;
+        this.birthdate = birthdate;
+    }
+
+    public int getIdAuthor() {
+        return idAuthor;
+    }
+
+    public void setIdAuthor(int idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+}
--- src/main/java/Models/Book.java
+++ src/main/java/Models/Book.java
@@ -57,5 +57,5 @@ public class Book {
     public void setIdAuthor(int idAuthor) {
         this.idAuthor = idAuthor;
     }
-    
+
 }
